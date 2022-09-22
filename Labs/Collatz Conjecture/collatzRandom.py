06ce6e67755b32d095e16b8702323a6204d6f179
GRJOMBAS <gomesg@lawrence.edu>
2022-09-22 11:48:31 -0500

    changes

--- Labs/Collatz Conjecture/collatzRandom.py
+++ Labs/Collatz Conjecture/collatzRandom.py
@@ -6,10 +6,8 @@ chain = 0
 while num != 1:
     if num % 2 == 0:
         num = num // 2
-        #print("Div by 2: ", num)
         chain += 1
     else:
         num = (num*3) + 1
-        #print("Times 3 Plus 1: ", num)
         chain += 1
 print("Length of Chain:", chain)
